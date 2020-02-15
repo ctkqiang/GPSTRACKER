03a5645a9fdcc37c511723a375702ca98c6006ae
John Melody Melissa And Tan Sin Dee <johnmelodyme@icloud.com>
2020-02-15 14:59:19 +0800

    Update Copyright

--- app/src/main/java/com/johnmelodyme/gpstracker/MainActivity.java
+++ app/src/main/java/com/johnmelodyme/gpstracker/MainActivity.java
@@ -1,18 +1,29 @@
 package com.johnmelodyme.gpstracker;
+/**
+ * @Copyright : Copyright © 2020 John Melody Melissa
+ * ********************************************************
+ * THIS APPLICATION IS MEANT FOR EDUCATIONAL PURPOSES ONLY
+ * IS NOT INTENT TO SOMETHING SINISTER OR MALICIOUS, I WILL
+ * NOT RESPONSIBLE IF MISUSE OF THIS TECHNOLOGY OR THE
+ * ABUSIVE OF USAGE OF THIS PROGRAMME.
+ * ********************************************************
+ * @AUTHOR: John Melody Melissa
+ * @REFERENCE: https://github.com/johnmelodyme/GPSTRACKER
+ * This application inspired by @Cindy, My Baby.
+ */
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-/**
- * @AUTHOR: John Melody Melissa
- * @REFERENCE: https://github.com/johnmelodyme/GPSTRACKER
-*/
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "GPSTRACKER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, MainActivity.TAG.toLowerCase() + MainActivity.class.getName() + "is Starting...");
     }
 }
