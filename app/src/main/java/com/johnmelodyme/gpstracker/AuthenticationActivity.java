30278e9bc26329a27c98d58302d165e7cc9cc595
John Melody Melissa And Tan Sin Dee <Johnmelodyme@icloud.com>
2020-02-15 16:29:13 +0800

    Server Setup<Firebase>; Authentication<>;

--- app/src/main/java/com/johnmelodyme/gpstracker/AuthenticationActivity.java
+++ app/src/main/java/com/johnmelodyme/gpstracker/AuthenticationActivity.java
@@ -12,15 +12,112 @@ package com.johnmelodyme.gpstracker;
  * This application inspired by @Cindy, My Baby.
  */
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class AuthenticationActivity extends AppCompatActivity {
     private static final String TAG = "GPSTRACKER";
+    private Button btnLogin, btnRegister;
+    private ProgressBar progressBar;
+    private EditText userEmail, userPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+        Log.d(TAG, "Authentication...");
+
+        // UI Declaration:
+        btnLogin = findViewById(R.id.Login);
+        btnRegister = findViewById(R.id.Register);
+        progressBar = findViewById(R.id.progressBar);
+        userEmail = findViewById(R.id.user_email);
+        userPassword = findViewById(R.id.user_password);
+
+        // Firebase Declaration:
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        // TODO REGISTRATION BUTTON LISTENER:
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Check Use Email and Password {format}
+                final String email, password, err_email, err_pass, weakpass;
+
+                // Get String from `EditText`
+                email = userEmail.getText()
+                        .toString()
+                        .trim();
+
+                password = userPassword.getText()
+                        .toString()
+                        .trim();
+
+                // Get String Res:
+                err_email = getResources().getString(R.string.pleaseenteremail);
+                err_pass = getResources().getString(R.string.pleaseenterpass);
+                weakpass = getResources().getString(R.string.weakP);
+
+                // Make it `invalid` if $user leave EditText blank or weak password:
+                if (TextUtils.isEmpty(email)){
+                    userEmail.setError(err_email);
+                    Log.d(TAG, err_email);
+                }
+
+                if (TextUtils.isEmpty(password)){
+                    if (password.length() < 6) {
+                        userPassword.setError(weakpass);
+                    } else {
+                        userPassword.setError(err_pass);
+                    }
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Create user Profile Firebase :
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(AuthenticationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> CREATE_AN_USER_ACCOUNT) {
+                                // Listen for accountability:
+                                if (CREATE_AN_USER_ACCOUNT.isSuccessful()){
+                                    Log.d(TAG, "Welcome " + email);
+                                    dispToast("Welcome " + email);
+                                    Intent goToTrackerActivity;
+                                    goToTrackerActivity = new Intent(AuthenticationActivity.this, TrackerActivity.class);
+                                    startActivity(goToTrackerActivity);
+                                    Log.d(TAG, "Redirecting to TrackingActivity.class");
+                                } else {
+                                    dispToast("failed to create user " + email);
+                                    Log.d(TAG,"failed to create user " + email );
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    public void dispToast(String s){
+        Toast.makeText(getApplicationContext(), s,
+                Toast.LENGTH_SHORT)
+                .show();
     }
 }
