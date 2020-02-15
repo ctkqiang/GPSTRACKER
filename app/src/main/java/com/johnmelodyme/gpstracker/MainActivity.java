270d80cbd9aaad172e4e5b3bdc1111f8a805ed4e
John Melody Melissa And Tan Sin Dee <Johnmelodyme@icloud.com>
2020-02-15 14:42:52 +0800

    Commit

---
+++ app/src/main/java/com/johnmelodyme/gpstracker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.johnmelodyme.gpstracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @AUTHOR: John Melody Melissa
+ * @REFERENCE: https://github.com/johnmelodyme/GPSTRACKER
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
