2a35e59d83ac82835cc603e14ecb8a31567f85b2
John Melody Melissa And Tan Sin Dee <Johnmelodyme@icloud.com>
2020-02-15 15:06:37 +0800

    Update Copyright

--- app/src/main/java/com/johnmelodyme/gpstracker/TrackerActivity.java
+++ app/src/main/java/com/johnmelodyme/gpstracker/TrackerActivity.java
@@ -17,13 +17,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 
-public class MainActivity extends AppCompatActivity {
+public class TrackerActivity extends AppCompatActivity {
     private static final String TAG = "GPSTRACKER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, MainActivity.TAG.toLowerCase() + MainActivity.class.getName() + "is Starting...");
+        Log.d(TAG, TrackerActivity.TAG.toLowerCase() + TrackerActivity.class.getName() + "is Starting...");
     }
 }
